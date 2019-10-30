24c76cfd526e37f272bda6f5ac80b85c31d197d3
morbiumlivonian <55694112+morbiumlivonian@users.noreply.github.com>
2019-10-30 11:26:53 +0530

    Update mobile.js

--- mobile.js
+++ mobile.js
@@ -1,3 +1,3 @@
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
-    window.location.href="mobile/index.html";
+    window.location.href="mobile.html";
 }
